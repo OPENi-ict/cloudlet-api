b0986557ea63d993c355e88ecc10075c080ba437
Philip O'Brien <obrien.philip.p@gmail.com>
2014-11-27 12:03:13 +0000

    linting run through

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,107 +2,21 @@
 
 module.exports = function(grunt) {
 
-    var tests       = 'test/**/*_test.js'
-    var build_tests = 'build/instrument/' + tests
-    var lib         = 'lib/**/*.js'
-
+    var lib         = 'lib/**/*.js';
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        clean: {
-            build:['build']
-        },
         jshint: {
             all: [lib],
-            options: grunt.file.readJSON('.jshintrc')
-        },
-        nodeunit: {
-            all: tests
-        },
-        watch : {
-            files : [ lib, tests ],
-            tasks : 'default'
-        },
-        plato: {
             options: {
-                title: 'Cloudlet API',
-                jshint: grunt.file.readJSON('.jshintrc')
-            },
-            metrics: {
-                files: {
-                    'build/metrics': [ lib ]
-                }
-            }
-        },
-        instrument : {
-            files : [lib],
-            options : {
-                lazy : true,
-                excludes : tests,
-                basePath : 'build/instrument/',
-                flatten : false
-            }
-        },
-        reloadTasks : {
-            rootPath : 'build/instrument/lib'
-        },
-        storeCoverage : {
-            options : {
-                dir : 'build/reports/'
-            }
-        },
-        makeReport : {
-            src : 'build/reports/**/*.json',
-            options : {
-                reporters : {
-                    'lcov'     :{dir:'build/reports/'},
-                    'cobertura':{dir:'build/reports/'}
-                },
-                print : 'detail'
-
-            }
-        },
-        coverage: {
-            options: {
-                thresholds: {
-                    'statements': 90,
-                    'branches'  : 90,
-                    'lines'     : 90,
-                    'functions' : 90
-                },
-                dir: 'reports',
-                root: 'build/'
-            }
-        },
-        required: {
-            libs: {
-                options: {
-                    install: true
-                },
-                src: [lib]
+                reporter: require('jshint-stylish'),
+                jshintrc: '.jshintrc'
             }
         }
-
     });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-qunit-cov');
-  grunt.loadNpmTasks('grunt-plato');
-  grunt.loadNpmTasks('grunt-istanbul');
-  grunt.loadNpmTasks('grunt-istanbul-coverage');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-config');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // Default task(s).
-  grunt.registerTask('test',     ['jshint']);
-  grunt.registerTask('cover',    ['clean:build', 'instrument', 'nodeunit', 'storeCoverage', 'makeReport']);
-  grunt.registerTask('default',  ['required',    'jshint',     'nodeunit' ]);
-  grunt.registerTask('jenkins',  ['jshint',      'cover',      'coverage',    'plato']);
-
+    grunt.registerTask('default',  ['jshint' ]);
 
 };
